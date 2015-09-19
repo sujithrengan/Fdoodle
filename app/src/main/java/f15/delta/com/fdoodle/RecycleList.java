45f44588fe29f651ea26b0e313dc5bc7283b5b92
shilpasweth <shilpasweth@gmail.com>
2015-09-19 18:39:46 +0530

    Fix -1 time

--- app/src/main/java/f15/delta/com/fdoodle/RecycleList.java
+++ app/src/main/java/f15/delta/com/fdoodle/RecycleList.java
@@ -1,5 +1,9 @@
 package f15.delta.com.fdoodle;
 
+
+
+
+import android.app.Application;
 import android.content.Context;
 import android.graphics.Color;
 import android.support.v7.widget.RecyclerView;
@@ -8,6 +12,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.Calendar;
 import java.util.GregorianCalendar;
@@ -59,6 +64,33 @@ public class RecycleList extends RecyclerView.Adapter<RecycleList.CustomViewHold
         }
     }
 
+    public String propergram(String word){
+
+
+        // word.toLowerCase();
+        String[] tempstr =word.split("_");
+        String tempstr2;
+
+        for(int i=0;i<tempstr.length;i++){
+            if((tempstr[i].charAt(0))>='A'&&(tempstr[i].charAt(0))<='Z') {
+                tempstr2 = (String.valueOf(tempstr[i].charAt(0)));
+            }
+            else {
+                tempstr2 = (String.valueOf(tempstr[i].charAt(0))).toUpperCase();
+            }
+            tempstr[i] = tempstr2.concat(tempstr[i].substring(1));
+            if (i != 0) {
+                word=word.concat(" ").concat(tempstr[i]);
+            } else {
+                word = tempstr[i];
+            }
+
+        }
+        return word;
+
+
+    }
+
     @Override
     public CustomViewHolder onCreateViewHolder(ViewGroup viewGroup, int i) {
         View view = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.list_single, null);
@@ -70,9 +102,14 @@ public class RecycleList extends RecyclerView.Adapter<RecycleList.CustomViewHold
     @Override
     public void onBindViewHolder(CustomViewHolder customViewHolder, int i) {
         int position=i;
-        int date=25;
+        int date=26;
+        String[] temp=present[position];
 
         timenow.set(Calendar.DATE,date);
+
+
+
+
 
 
 
@@ -82,41 +119,48 @@ public class RecycleList extends RecyclerView.Adapter<RecycleList.CustomViewHold
         Calendar time6 = new GregorianCalendar(TimeZone.getTimeZone("GMT+5:30"));
         time6.set(timenow.get(Calendar.YEAR), 8, time[position][3], time[position][4], time[position][5]);
         //TextView txtTitle = (TextView) rowView.findViewById(R.id.Event);
-        customViewHolder.Event.setText(present[position][0]);
+        customViewHolder.Event.setText(propergram(temp[0]));
         //txtTitle = (TextView) rowView.findViewById(R.id.Location);
-        customViewHolder.Location.setText(present[position][1]);
+        customViewHolder.Location.setText(propergram(temp[1]));
         //txtTitle = (TextView) rowView.findViewById(R.id.Cate);
-        customViewHolder.Cate.setText(present[position][2]);
+        customViewHolder.Cate.setText(propergram(temp[2]));
         //txtTitle = (TextView) rowView.findViewById(R.id.Time);
 
         if (time5.after(timenow)) {
-            if ((timenow.get(Calendar.MINUTE) <= time[position][2])) {
-                a = (-timenow.get(Calendar.HOUR_OF_DAY) + time[position][1]);
-                b = (time[position][2] - timenow.get(Calendar.MINUTE));
+           // if ((timenow.get(Calendar.MINUTE) <= time[position][2])) {
+                a =((int)((time5.getTimeInMillis() - timenow.getTimeInMillis())/3600000)); //(-timenow.get(Calendar.HOUR_OF_DAY) + time[position][1]);
+                b =((int)((time5.getTimeInMillis() - timenow.getTimeInMillis())/60000))%60; //(time[position][2] - timenow.get(Calendar.MINUTE));
 
-            } else {
+          // }
+            /*else {
                 a = (-timenow.get(Calendar.HOUR_OF_DAY) + time[position][1] - 1);
                 b = (60 - timenow.get(Calendar.MINUTE) + time[position][2]);
 
-            }
-            if ((timenow.get(Calendar.HOUR) > time[position][1])) {
+            }*/
+            /*if ((timenow.get(Calendar.HOUR) > time[position][1])) {
                 a+= 24;
                 //b = (time[position][2] - timenow.get(Calendar.MINUTE));
-            }
+            }*/
 
             customViewHolder.Time.setText("begins in " + a/*timenow.get(Calendar.YEAR)*/ + " hours " + b + " mins ");
-        } else {
-            if ((timenow.get(Calendar.MINUTE) <= time[position][5])) {
-                c = (-timenow.get(Calendar.HOUR_OF_DAY) + time[position][4]);
-                d = (time[position][5] - timenow.get(Calendar.MINUTE));
-            } else {
+        }
+        else if (time6.before(timenow)) {
+            // if ((timenow.get(Calendar.MINUTE) <= time[position][2])) {
+            customViewHolder.Time.setText("ends in 0 hours 0 mins ");
+        }
+        else {
+            //if ((timenow.get(Calendar.MINUTE) <= time[position][5])) {
+                c = ((int)((time6.getTimeInMillis() - timenow.getTimeInMillis())/3600000));//(-timenow.get(Calendar.HOUR_OF_DAY) + time[position][4]);
+                d =((int)((time6.getTimeInMillis() - timenow.getTimeInMillis())/60000))%60; //(time[position][5] - timenow.get(Calendar.MINUTE));
+           // }
+          /* else {
                 c = (-timenow.get(Calendar.HOUR_OF_DAY) + time[position][4] - 1);
                 d = (60 - timenow.get(Calendar.MINUTE) + time[position][5]);
             }
             if ((timenow.get(Calendar.HOUR) > time[position][4])) {
                 c+= 24;
                 //b = (time[position][2] - timenow.get(Calendar.MINUTE));
-            }
+            }*/
             customViewHolder.Time.setText("ends in " + c/*timenow.get(Calendar.YEAR)*/ + " hours " + d + " mins ");
         }
     }
